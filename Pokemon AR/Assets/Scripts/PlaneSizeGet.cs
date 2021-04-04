ed87a6f154629fd01329fe799e588eaca314cf86
Wuggie <kawog17@student.sdu.dk>
2021-04-04 16:51:44 +0200

    Get plane size script
    Made a script that can get the size of a plane from its scale, this can be implemented into the code Anders wrote, but i did not do it initially, because i want to make sure we're on the same track before just putting it in there.

---
+++ Pokemon AR/Assets/Scripts/PlaneSizeGet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneSizeGet : MonoBehaviour
+{
+    //this should be implemented in PlaneGenerationCalibration script, but isn't yet, just to make sure it runs, when it gets implemented (probably with a coroutine, delete this script)
+    public float planeSizeX = 0;
+    public float planeSizeZ = 0;
+    public GameObject plane;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //starts a coroutine, this should be done in PlanegenerationCalibrationscript when the plane size is set  (doesn't need a coroutine this is to make a wait time).
+        StartCoroutine(planeSet());
+    }
+
+    public IEnumerator planeSet()
+    {
+        Debug.Log("Running planeset IEnumerator");
+        yield return new WaitForSeconds(5);
+        getPlaneSize();
+    }
+
+    public void getPlaneSize()
+    {
+        Debug.Log("running GetPlaneSize");
+        planeSizeX = plane.transform.localScale.x * 10;
+        planeSizeZ = plane.transform.localScale.z * 10;
+    }
+}
