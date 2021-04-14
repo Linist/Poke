2e7edc02163ae139dbcf5eea11e845794e90d4f0
Michelle Larsen <micch15@student.sdu.dk>
2021-04-14 12:19:36 +0200

    Import of stats from excel file

---
+++ Pokemon AR/Assets/Scripts/PokemonCreate.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public enum Elements
+{
+    Fire,
+    Water,
+    Grass,
+    Electric
+}
+
+[System.Serializable]
+public class PokemonCreate
+{
+
+    public string name;
+    public int level;
+    public int baseAttack;
+    public int baseDefence;
+    public int hp;
+    public int maxHp;
+    public Elements element;
+    public List<Move> moves;
+
+    public Pokemon(string name, int level, int baseAttack,
+        int baseDefence, int hp, Elements element, List<Move> moves)
+    {
+        this.level = level;
+        this.baseAttack = baseAttack;
+        this.baseDefence = baseDefence;
+        this.name = name;
+        this.hp = hp * level;
+        this.maxHp = hp * level;
+        this.element = element;
+        this.moves = moves;
+    }
+
+
+    public int Attack(Pokemon enemy)
+    {
+        
+        int attack = baseAttack * level;
+        attack = CalculateElementalEffects(attack, enemy.element);
+        int defence = enemy.CalculateDefence();
+        int damage = attack - defence;
+        if (damage < 0)
+            return 0;
+        else
+        {
+            enemy.ApplyDamage(damage);
+            return damage;
+        }
+    }
+
+
+    public int CalculateDefence()
+    {
+        return baseDefence * level;
+    }
+
+    /// Calculates elemental effect, check table at https://bulbapedia.bulbagarden.net/wiki/Type#Type_chart for a reference
+
+    public int CalculateElementalEffects(int damage, Elements enemyType)
+    {
+        if (element == Elements.Water && enemyType == Elements.Fire ||
+            element == Elements.Grass && enemyType == Elements.Water ||
+            element == Elements.Fire && enemyType == Elements.Grass
+            )
+        {
+            
+            return damage * 2;
+        }
+        if (element == Elements.Fire && enemyType == Elements.Water ||
+            element == Elements.Water && enemyType == Elements.Grass ||
+            element == Elements.Grass && enemyType == Elements.Fire
+            )
+        {
+           
+            return damage / 2;
+        }
+        return damage;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        hp -= damage;
+    }
+
+    public void Restore()
+    {
+        hp = maxHp;
+    }
+}
+
---
+++ Pokemon AR/Assets/Scripts/PokemonStatsImporter.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UnityEngine;
+
+class PokemonStatsImporter
+{
+
+    public static Pokemon Create(int level, string name)
+    {
+
+        //read pokedex file and import
+        using (var reader = new StreamReader(@"Assets\Pokedex.csv"))
+        {
+            // Skipping first line
+            reader.ReadLine();
+
+            // Reading lines 
+            while (!reader.EndOfStream)
+            {
+                // Store values 
+                String pokeLine = reader.ReadLine();
+                String[] values = pokeLine.Split(',');
+
+                // finding corresponding pokemon
+                if (values[2].ToLower() == name.ToLower())
+                {
+                    
+                    return InstantiatePokemon(level, values);
+                }
+            }
+        }
+
+
+    }
+
+    static Pokemon InstantiatePokemon(int level, String[] values)
+    {
+        // Get the element from the pokemon info
+        Elements element;
+        switch (values[10])
+        {
+            case "Fire":
+                element = Elements.Fire;
+                break;
+            case "Water":
+                element = Elements.Water;
+                break;
+            case "Grass":
+                element = Elements.Grass;
+                break;
+            case "Electric"
+                element = Elements.Electric;
+                break;
+        }
+
+        // pokemon moves
+        List<Move> moves = new List<Move>();
+        moves.Add(new Move(values[13]));
+        if (values[14] != "")
+            moves.Add(new Move(values[14]));
+
+        // always two possible moves
+        else
+            moves.Add(new Move("Scratch"));
+
+        // create and return the pokemon 
+        return new Pokemon(values[2], level, int.Parse(values[4]), int.Parse(values[5]), int.Parse(values[3]), element, moves);
+    }
+
+    public static Pokemon CreateRandom()
+    {
+        // Store pokedex lines ind string array
+        String[] lines = File.ReadAllLines(@"Assets\Pokedex.csv");
+        // create randome pokemon
+        String randomLine = lines[UnityEngine.Random.Range(1, lines.Length)];
+        // separate stats
+        String[] values = randomLine.Split(',');
+        // Initialize and return pokemon
+        return InstantiatePokemon(UnityEngine.Random.Range(1, 11), values);
+    }
+}
