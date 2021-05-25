d57f0df56b404ce5b97ce880c9564c9a4b37d29d
Anders Gaarsdal <angaa19@student.sdu.dk>
2021-05-25 11:28:35 +0200

    Pokemon cards can now be scanned with a reference to be selected later

---
+++ Pokemon AR/Assets/Scripts/ScanningPokemon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanningPokemon : MonoBehaviour
+{
+    public string pokemon1, pokemon2, pokemon3;
+
+    string pokeName;
+
+    public void FoundPokemon(GameObject pokemon)
+    {
+        pokeName = pokemon.transform.GetChild(0).name;
+
+        if (pokemon1 == "")
+        {
+            pokemon1 = pokeName;
+        }
+        else if (pokemon2 == "" && pokeName != pokemon1)
+        {
+            pokemon2 = pokeName;
+        }
+        else if (pokemon3 == "" && pokeName != pokemon2)
+        {
+            pokemon3 = pokeName;
+        }
+
+        Debug.Log("Pokemon1 is:" + pokemon1 + ", Pokemon2 is:" + pokemon2 + ", Pokemon3 is:" + pokemon3);
+    }
+}
