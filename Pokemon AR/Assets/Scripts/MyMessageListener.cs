2a9d56a3839ca82e2075997441b20bc54ec920d1
Wuggie <wuggie88@yahoo.co.uk>
2021-05-25 09:35:42 +0200

    Added messageListener script for arduino connection

---
+++ Pokemon AR/Assets/Scripts/MyMessageListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMessageListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Invoked on recieved data from the serial device
+    void onMessageArrived(string msg)
+    {
+        Debug.Log("Arried: " + msg);
+    }
+
+    /* Invoked when a connect/disconnect even occurs. The parameter "success"
+      will be "true" upon connection, and "false" upon Disconecction or
+      failure to connect
+      */ 
+        
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device Connected" : "Device Disconnected");
+    }
+}
