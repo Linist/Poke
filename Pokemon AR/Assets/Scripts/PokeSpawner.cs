48bd9d10dfcff521cf740c26c7e7219a9ea6325e
Anders Gaarsdal <angaa19@student.sdu.dk>
2021-05-26 11:31:16 +0200

    Made the grass be able to generate random pokemon

---
+++ Pokemon AR/Assets/Scripts/PokeSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokeSpawner : MonoBehaviour
+{
+    
+    public Pokemon enemy;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        int range = Random.Range(0, 10);
+        if (range == 0)
+            enemy = PokemonStatsImporter.CreateRandom();
+    }
+}
